653c8d471e67423f87efa63db899dad25a162ddd
zhangzheng567 <zhangzheng567@126.com>
2018-09-20 00:22:06 +0800

    回弹

---
+++ app/src/main/java/com/example/zhangzheng/myapplication1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.zhangzheng.myapplication1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button){
+            Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+            startActivity(intent);
+
+        }
+    }
+}
